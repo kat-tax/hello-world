a51b900fe56f4c4ec26ef023b4d0b73949047293
Syntax <2079305+TheUltDev@users.noreply.github.com>
2022-05-08 21:28:38 -0500

    Misc navigation stuff

--- src/interface/layout/Navigation.tsx
+++ src/interface/layout/Navigation.tsx
@@ -1,7 +1,7 @@
+import {useColorScheme, useWindowDimensions} from 'react-native';
 import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {useDarkMode} from 'interface/hooks/useDarkMode';
 import {Settings} from 'interface/stacks/Settings';
 import {Home} from 'interface/stacks/Home';
 
@@ -9,11 +9,25 @@ const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 export function Navigation() {
+  const scheme = useColorScheme();
+  const screen = useWindowDimensions();
   return (
-    <NavigationContainer theme={useDarkMode() ? DarkTheme : DefaultTheme}>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} options={{title: 'Hello App Display Name'}}/>
-        <Drawer.Screen name="Settings" component={Settings}/>
+    <NavigationContainer
+      theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Drawer.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          drawerType: screen.width >= 800 ? 'permanent' : 'front',
+        }}>
+        <Drawer.Screen
+          name='Home'
+          component={Home}
+          options={{title: 'Hello App Display Name'}}
+        />
+        <Drawer.Screen
+          name='Settings'
+          component={Settings}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
